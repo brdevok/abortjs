9f9391b3a78f10c31c280bc13dd147165f54d162
Braian Rosas <braianrosas@MMBA-B506.local>
2022-06-22 12:23:16 -0300

    Create return type for abort callback

--- src/abort-js/abort-js.types.ts
+++ src/abort-js/abort-js.types.ts
@@ -1,6 +1,7 @@
 export type Controllers = Record<string, AbortController>;
 
-export type AbortCollectionResults = Record<string, Promise<unknown> | unknown>;
+export type AbortCallbackResult = Promise<unknown> | unknown;
+export type AbortCollectionResults = Record<string, AbortCallbackResult>;
 
-export type AbortCallback = (signal: AbortSignal) => Promise<unknown> | unknown;
+export type AbortCallback = (signal: AbortSignal) => AbortCallbackResult;
 export type AbortCollection = Array<[string, AbortCallback]>;
